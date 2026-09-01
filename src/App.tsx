fdf98f7651a0cf8c7c4bde9162528264762b107f
aionmanifold-svg <aionmanifold@gmail.com>
2026-09-01 05:39:18 +0000

    Initialize React + TypeScript app with Vite and ESLint configuration

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Aion Manifold</h1>
+        <p>WebMCP Research + Governance OS</p>
+        <p>Core tools: research state, objective setting, findings, approval requests</p>
+      </div>
+    </>
+  )
+}
+
+export default App
